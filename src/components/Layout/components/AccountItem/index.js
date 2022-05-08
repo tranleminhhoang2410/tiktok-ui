ab5c482f401978976ce1f5690a23f73d6dd7e363
Le Minh Hoang Tran <82258999+tranleminhhoang2410@users.noreply.github.com>
2022-05-08 23:09:20 +0700

    build ui header #2

---
+++ src/components/Layout/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from './AccountItem.module.scss'
+
+const cx = classNames.bind(styles)
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="https://huyhoanhotel.com/wp-content/uploads/2016/05/765-default-avatar.png" alt="Hoa" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
