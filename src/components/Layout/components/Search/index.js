d00cb49c93e385ad1949ff65ecfb3b00789d14e5
Le Minh Hoang Tran <82258999+tranleminhhoang2410@users.noreply.github.com>
2022-05-21 22:19:22 +0700

    build logic header #3

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -9,6 +9,7 @@ import {
 import HeadlessTippy from '@tippyjs/react/headless';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '../AccountItem';
+import { useDebounce } from '~/hooks'
 
 import styles from './Search.module.scss'
 import classNames from 'classnames/bind';
@@ -21,17 +22,19 @@ function Search() {
     const [showResult, setShowResult] = useState(true)
     const [loading, setLoading] = useState(false)
 
+    const debounced = useDebounce(searchValue, 500)
+
     const inputRef = useRef()
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([])
             return
         }
 
         setLoading(true)
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then(res => res.json())
             .then((res) => {
                 setSearchResult(res.data)
@@ -40,7 +43,7 @@ function Search() {
             .catch(() => {
                 setLoading(false)
             })
-    }, [searchValue])
+    }, [debounced])
 
     const handleClear = () => {
         setSearchValue('')
