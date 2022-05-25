ed01a2beb0ad067857a35ea01f0e058c7ecc6223
Le Minh Hoang Tran <82258999+tranleminhhoang2410@users.noreply.github.com>
2022-05-25 10:56:02 +0700

    introduce axios

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -11,6 +11,7 @@ import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '../AccountItem';
 import { useDebounce } from '~/hooks'
 
+import * as searchServices from '~/apiServices/searchServices'
 import styles from './Search.module.scss'
 import classNames from 'classnames/bind';
 
@@ -32,17 +33,17 @@ function Search() {
             return
         }
 
-        setLoading(true)
+        const fetchApi = async () => {
+            setLoading(true);
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
-            .then(res => res.json())
-            .then((res) => {
-                setSearchResult(res.data)
-                setLoading(false)
-            })
-            .catch(() => {
-                setLoading(false)
-            })
+            const result = await searchServices.search(debounced);
+
+            setSearchResult(result);
+            setLoading(false);
+        };
+
+        fetchApi();
+
     }, [debounced])
 
     const handleClear = () => {
