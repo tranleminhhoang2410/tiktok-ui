d20b924f6b0cd572f8c91502af8bc671bccf99e3
Le Minh Hoang Tran <82258999+tranleminhhoang2410@users.noreply.github.com>
2022-06-15 10:59:25 +0700

    reduce index.js file

---
+++ src/layouts/HeaderOnly/HeaderOnly.js
@@ -0,0 +1,14 @@
+import Header from '~/layouts/components/Header';
+
+function HeaderOnly({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="content">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default HeaderOnly;
