26dfe97f156e4d567b4b84590550d3a1bd1e7ea5
Le Minh Hoang Tran <82258999+tranleminhhoang2410@users.noreply.github.com>
2022-06-17 14:00:03 +0700

    build sidebar ui #1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { publicRoutes } from '~/routes'
-import DefaultLayout from '~/layouts'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import DefaultLayout from '~/layouts';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          {publicRoutes.map((route, index) => {
-            let Layout = DefaultLayout
+function App () {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
 
-            if (route.layout) {
-              Layout = route.layout
-            } else if (route.layout === null) {
-              Layout = Fragment
-            }
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
 
-            const Page = route.component
-            return <Route
-              key={index}
-              path={route.path}
-              element={<Layout>
-                <Page />
-              </Layout>} />
-          })}
-        </Routes>
-      </Router>
-    </div>
-  );
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
